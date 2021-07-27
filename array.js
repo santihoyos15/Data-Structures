9a5b8f40a6466c587b372bebcd29ff85726a6828
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-07-27 11:36:19 -0500

    First commit

---
+++ array.js
@@ -0,0 +1,64 @@
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index){
+        return this.data[index];
+    }
+
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop(){
+        const lastItem = this.length - 1;
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    shift(){
+        const firstItem = this.data["0"];
+
+        for (let i = 0; i < this.length; i++){
+            let rightItem = this.data[i + 1];
+            this.data[i] = rightItem;
+        }
+
+        delete this.data[this.length - 1];
+        this.length--;
+        return firstItem;
+    }
+
+    unshift(item){
+        this.push(0);
+
+        for (let i = 0; i < this.length; i++){
+            const rightItem = this.data[i + 1];
+            this.data[i] = rightItem;
+        }
+
+        this.data[0] = item;
+
+        this.length++;
+        return this.length;
+    }
+}
+
+const nums = new MyArray();
+
+nums.push(1);
+nums.push(2);
+nums.push(3);
+nums.push(4);
+nums.push(5);
+nums.push(6);
+nums.unshift(0);
+
+for (let i = 0; i < nums.length; i++){
+    console.log(nums.data[i]);
+}
