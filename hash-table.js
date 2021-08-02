01c90911f10458f318e2636d088d5d1c1f8f1216
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-08-02 16:57:16 -0500

    Hash function implemented.

--- hash-table.js
+++ hash-table.js
@@ -2,6 +2,16 @@ class HashTable {
     constructor(size){
         this.data = new Array(size);
     } 
+
+    hashMethod(key){
+        let hash = 0;
+        for (let i = 0; key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+
+        return hash;
+    }
+    
 }
 
 const myHash = new HashTable(50);
