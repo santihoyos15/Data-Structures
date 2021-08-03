304fe090e4af9801469407a8c25f2136aac8c2b9
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-08-03 14:09:13 -0500

    SinglyLinkedList constructor implemented.

--- linked-lists/SinglyLinkedList.js
+++ linked-lists/SinglyLinkedList.js
@@ -1,3 +1,9 @@
 class SinglyLinkedList {
-    
+    constructor(data){
+        this.head = new Node(data);
+
+        this.tail = this.head; //Assigning tail to head. 
+
+        this.length = 1;
+    }
 }
