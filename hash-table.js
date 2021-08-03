51d426ee0df4a470414731ea99dc4782cc403f50
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-08-03 09:25:22 -0500

    Get method implemented.

--- hash-table.js
+++ hash-table.js
@@ -22,6 +22,25 @@ class HashTable {
         return this.data;
     }
 
+    get(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address]; //Getting the list[] of key, value lists[].
+        if (currentBucket){
+            for (let i = 0; i < currentBucket.length; i++){
+                if (currentBucket[i][0] === key){
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return null;
+    }
+
 }
 
 const myHash = new HashTable(50);
+
+myHash.set("Santiago", 19);
+myHash.set("Vanessa", 23);
+const value = myHash.get("Santiago");
+const value2 = myHash.get("Vanessa");
+console.log(value, value2);
